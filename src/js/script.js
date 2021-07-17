aa7b9f37fb28dc9d7a7fcc1edfc145d8190f66a4
Sens3ii <densteeper@gmail.com>
2021-07-17 18:08:35 +0600

    html&styles&js: catalog section

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
+//=============================================================
+// Slider
+//=============================================================
+
 const slider = tns({
 	container: ".slider",
 	items: 1,
@@ -18,4 +22,72 @@ document.querySelector(".prev").addEventListener("click", function () {
 });
 document.querySelector(".next").addEventListener("click", function () {
 	slider.goTo("next");
+});
+
+//=============================================================
+// Tabs
+//=============================================================
+
+const tabsParent = document.querySelector(".catalog__tabs");
+const tabs = document.querySelectorAll(".catalog__tab");
+const tabsContent = document.querySelectorAll(".catalog__content");
+
+function hideTabContent() {
+	tabsContent.forEach((content) => {
+		content.classList.remove("catalog__content_active");
+	});
+	tabs.forEach((tab) => {
+		tab.classList.remove("catalog__tab_active");
+	});
+}
+
+function showTabContent(i = 0) {
+	tabsContent[i].classList.add("catalog__content_active");
+	tabs[i].classList.add("catalog__tab_active");
+}
+
+hideTabContent();
+showTabContent(0);
+
+tabsParent.addEventListener("click", (e) => {
+	const target = e.target.parentNode;
+	if (target && target.classList.contains("catalog__tab")) {
+		tabs.forEach((tab, i) => {
+			if (target == tab) {
+				hideTabContent();
+				showTabContent(i);
+			}
+		});
+	}
+});
+
+//=============================================================
+// Items
+//=============================================================
+const catalog = document.querySelector(".catalog");
+
+// function to flip items on their initial position
+function flipItems() {
+	const itemContents = document.querySelectorAll(".catalog-item__content");
+	const itemLists = document.querySelectorAll(".catalog-item__list");
+	itemContents.forEach((itemContent) => {
+		itemContent.classList.remove("catalog-item__content_active");
+		itemContent.classList.add("catalog-item__content_active");
+	});
+	itemLists.forEach((itemList) => {
+		itemList.classList.remove("catalog-item__list_active");
+	});
+}
+
+flipItems();
+
+// adding event listener to buttons cancel and more
+catalog.addEventListener("click", (e) => {
+	const target = e.target;
+	if (target && (target.classList.contains("catalog-item__back") || target.classList.contains("catalog-item__link"))) {
+		e.preventDefault();
+		const item = target.parentNode.parentNode; // getting item from its button
+		item.querySelector(".catalog-item__content").classList.toggle("catalog-item__content_active");
+		item.querySelector(".catalog-item__list").classList.toggle("catalog-item__list_active");
+	}
 });
