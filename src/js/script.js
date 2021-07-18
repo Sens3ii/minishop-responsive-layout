672af3e95713293f21519b33877e5b58f76e1020
Sens3ii <densteeper@gmail.com>
2021-07-18 12:37:29 +0600

    added: mail post

--- src/js/script.js
+++ src/js/script.js
@@ -175,4 +175,21 @@ window.addEventListener("DOMContentLoaded", () => {
 	validateForms("#order form");
 
 	$("input[name=phone]").mask("+7 (999) 999-99-99");
+
+	// Data send
+	$("form").submit(function (e) {
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize(),
+		}).done(function () {
+			$(this).find("input").val("");
+			$("#consultation, #order").fadeOut();
+			$(".overlay, #thanks").fadeIn("slow");
+
+			$("form").trigger("reset");
+		});
+		return false;
+	});
 });
