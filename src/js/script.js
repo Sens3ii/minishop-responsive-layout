a02ccfad72a8a6d30a610d874f130589cd19395e
Sens3ii <densteeper@gmail.com>
2021-07-18 12:59:04 +0600

    scroll up

--- src/js/script.js
+++ src/js/script.js
@@ -192,4 +192,20 @@ window.addEventListener("DOMContentLoaded", () => {
 		});
 		return false;
 	});
+
+	// Smooth scroll and pageup
+
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 1600) {
+			$(".pageup").fadeIn();
+		} else {
+			$(".pageup").fadeOut();
+		}
+	});
+
+	$("a[href^='#']").click(function () {
+		const _href = $(this).attr("href");
+		$("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+		return false;
+	});
 });
